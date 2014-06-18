284fa6105c96cf2a490a81dacec35946af9fe5e6
tr4656 <yifanj1@yahoo.com>
2014-06-17 22:51:02 -0700

    Added Slider

--- js/default.js
+++ js/default.js
@@ -5,3 +5,29 @@
 
 
 // JS functions and initiations go here...
+
+//Slider
+$(function(){
+	$("#two-prong-slider").noUiSlider({	
+		start: 3,
+		step: 1,
+		range: {
+			'min': 0,
+			'max': 9
+		},
+		serialization: {
+			lower: [
+			// Write the values to
+			// an input with id 'field'
+				$.Link({
+					target: $("#numTwo"),
+					decimals: 0
+				})
+			],
+		},
+		format: {
+			decimals: 0
+		}
+
+	});
+});
