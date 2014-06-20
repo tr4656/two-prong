4e8356aea56dfe82c535f7e04685919d0847d59a
tr4656 <tr4656@icebrg.us>
2014-06-19 20:44:24 -0700

    Added everything but JS

---
+++ js/default-ck.js
@@ -0,0 +1,6 @@
+/* ==========================================================
+  Base Default JavaScript
+  -- Table of Contents --
+*/// JS functions and initiations go here...
+//Slider
+$(function(){$("#two-prong-slider").noUiSlider({start:3,step:1,range:{min:0,max:12},serialization:{lower:[$.Link({target:$("#numTwo"),decimals:0})]},format:{decimals:0}});$("#chain-slider").noUiSlider({start:1,step:1,range:{min:1,max:4},serialization:{lower:[$.Link({target:$("#numChain"),decimals:0})]},format:{decimals:0}})});
--- js/default.js
+++ js/default.js
@@ -8,12 +8,12 @@
 
 //Slider
 $(function(){
-	$("#two-prong-slider").noUiSlider({	
+	$("#two-prong-slider").noUiSlider({
 		start: 3,
 		step: 1,
 		range: {
 			'min': 0,
-			'max': 9
+			'max': 12
 		},
 		serialization: {
 			lower: [
@@ -21,6 +21,28 @@ $(function(){
 			// an input with id 'field'
 				$.Link({
 					target: $("#numTwo"),
+					decimals: 0
+				})
+			],
+		},
+		format: {
+			decimals: 0
+		}
+
+	});
+	$("#chain-slider").noUiSlider({
+		start: 1,
+		step: 1,
+		range: {
+			'min': 1,
+			'max': 4
+		},
+		serialization: {
+			lower: [
+			// Write the values to
+			// an input with id 'field'
+				$.Link({
+					target: $("#numChain"),
 					decimals: 0
 				})
 			],
